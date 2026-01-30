d1053fe014bccca2ab3a0167854399c6db1997cf
Pedro Morissawa <example@example.com>
2026-01-30 11:12:30 -0300

    Chunks of Bytecode

--- clox/scripts/clox.sh
+++ clox/scripts/clox.sh
@@ -2,7 +2,7 @@
 
 if test $1 = "build"; then
     echo "Compiling clox"
-    gcc -o build/clox main.c chunk.c memory.c debug.c -Wall -Wextra
+    gcc -o build/clox main.c chunk.c memory.c debug.c value.c -Wall -Wextra
 elif test $1 = "run"; then
     ./build/clox
 else
