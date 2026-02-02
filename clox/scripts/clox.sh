b5dcd2b348ce0f68fa332cf47b34429fa5e28f87
Pedro Morissawa <example@example.com>
2026-02-02 17:20:45 -0300

    wip: scanning

--- clox/scripts/clox.sh
+++ clox/scripts/clox.sh
@@ -2,7 +2,7 @@
 
 if test $1 = "build"; then
     echo "Compiling clox"
-    gcc -o build/clox main.c chunk.c memory.c debug.c value.c vm.c -Wall -Wextra
+    gcc -o build/clox main.c chunk.c memory.c debug.c value.c vm.c compiler.c scanner.c -Wall -Wextra
 elif test $1 = "run"; then
     ./build/clox
 else
